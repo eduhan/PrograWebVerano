afd06e137d1814b345eb8491a07ee0ef22ba7725
Alejandra Quemada Patino <ale_kemada@hotmail.com>
2014-06-27 16:23:41 -0500

    Primer commit

---
+++ practica1/guardar.php
@@ -0,0 +1,31 @@
+<?php
+	echo "Hola";
+	
+	echo "<br/>";
+	echo "<br/>";
+	echo "Goles equipo 1 local: " . $_POST['equipo1_local'];
+	echo "<br/>";
+	echo "Goles equipo 2 visitante: " . $_POST['equipo2_visitante'];
+	
+	echo "<br/>";
+	echo "<br/>";
+	echo "Goles equipo 3 local: " . $_POST['equipo3_local'];
+	echo "<br/>";
+	echo "Goles equipo 4 visitante: " . $_POST['equipo4_visitante'];
+	
+	echo "<br/>";
+	echo "<br/>";
+	echo "Equipo elegido 5 local: " . $_POST['equipos1_local'];
+	echo "<br/>";
+	echo "Goles equipo 6 local: " . $_POST['equipo5_local'];
+	
+	echo "<br/>";
+	echo "<br/>";
+	echo "Equipo elegido visitante: " . $_POST['equipos2_local'];
+	echo "<br/>";
+	echo "Goles equipo visitante: " . $_POST['equipo6_visitante'];
+	
+
+
+
+?>
